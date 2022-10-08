c0ba2cfff4268b42ce6d162306faa6e74ef14251
Vladyslav <vkadyrov@protonmail.com>
2022-10-08 16:18:06 -0400

    commit tree 2680d4c046963309b85e273c7244ab94b4d3a714

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    const p_text = document.querySelector("#text");
+    p_text.textContent = "This is really cool!";
+});
+
